c8dd93a1671f4249afddede9612963f8ef1e8b64
root <root@localhost.localdomain>
2019-12-11 18:33:49 +0800

    modify download method

--- ximalaya/downloadximala_vip.py
+++ ximalaya/downloadximala_vip.py
@@ -18,6 +18,12 @@ KW_URL_PARA="?appid=0&categoryId=-1&condition=relation&core=album&device=android
 useragent="ting_6.6.21(Redmi+Note+4,Android23)"
 host="audio.pay.xmcdn.com"
 
+def progressbar(processnum,totalnum):
+    scale=int(float(processnum)/totalnum*100)
+    j = '#'*scale
+    print('【'+j+'】->'+str(scale)+'%\r'),
+    sys.stdout.flush() 
+
 
 keyword=sys.argv[1]
 search_kw_url=SEARCH_KW_URL_PREFIX+KW_URL_PARA.format(searchword=keyword,userid=UID)
@@ -60,13 +66,26 @@ resourcelist=rjson['tracks']['list']
 #print resourcelist
 #exit(0)
 
+urllist=[]
+with open('Untitled.csv','r') as fp:
+    for line in fp:
+        url=line.split(',')[0]
+        if '.m4a' in url:
+            urllist.append(url)
+
 #for pc cookie
 print("there are total "+str(len(resourcelist))+" ji")
 resource_name_trackid=[]
-for resource in resourcelist:
+i=1
+for resource,url in zip(resourcelist,urllist):
 	title=resource['title']
 	trackId=resource['trackId']
 	uid=resource['uid']
+	
+	
+	
+	'''
+	#手动输出下载地址
 	try:
 		url=input("skip this item,please input number 0,now begin to download : "+title.encode('utf-8')+"\n")	
 	
@@ -76,11 +95,18 @@ for resource in resourcelist:
 		
 	if url==0:
 		continue
+	'''
+
 	
 	cmd="curl -s -o '"+title.encode('utf-8')+".m4a' " + '-H "user-agent:'+useragent+'"'+' -H "Host:'+host+'"' +" -b loginmobile.txt '"+ url+"'"
 	
 	ret=os.popen(cmd)	
 
+
+	
+        progressbar(i,len(resourcelist))
+	i+=1
+	
 	#resource_name_trackid.append((title,trackId))
 	
 	'''
